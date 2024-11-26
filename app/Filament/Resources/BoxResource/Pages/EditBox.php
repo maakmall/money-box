e328045434835a366a38f8963a83c42bebfedaca
Muhammad Akmal <makmal316@gmail.com>
2024-11-27 00:22:10 +0700

    Update saved notification, breadcrumb and redirect

--- app/Filament/Resources/BoxResource/Pages/EditBox.php
+++ app/Filament/Resources/BoxResource/Pages/EditBox.php
@@ -4,16 +4,34 @@ namespace App\Filament\Resources\BoxResource\Pages;
 
 use App\Filament\Resources\BoxResource;
 use Filament\Actions;
+use Filament\Notifications\Notification;
 use Filament\Resources\Pages\EditRecord;
 
 class EditBox extends EditRecord
 {
     protected static string $resource = BoxResource::class;
 
-    protected function getHeaderActions(): array
+    protected function getRedirectUrl(): string
+    {
+        return $this->getResource()::getUrl('view', ['record' => $this->getRecord()]);
+    }
+
+    protected function getSavedNotification(): ?Notification
+    {
+        return Notification::make()
+            ->success()
+            ->title('Box Updated')
+            ->body('The box has been saved successfully');
+    }
+
+    public function getBreadcrumbs(): array
     {
         return [
-            Actions\DeleteAction::make(),
+            $this->getResource()::getUrl() => $this->getResource()::getBreadcrumb(),
+            $this->getResource()::getUrl('view', [
+                'record' => $this->getRecord()
+            ]) => 'View',
+            'Edit'
         ];
     }
 }
