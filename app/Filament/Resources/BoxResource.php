bf5e060840d172037e70d357e17be72ed227c6d8
Muhammad Akmal <makmal316@gmail.com>
2024-11-27 00:22:50 +0700

    Refactor box resource

--- app/Filament/Resources/BoxResource.php
+++ app/Filament/Resources/BoxResource.php
@@ -56,66 +56,7 @@ class BoxResource extends Resource
                     ->columns()
             ]);
     }
-
-    public static function table(Table $table): Table
-    {
-        return $table
-            ->columns([
-                Tables\Columns\TextColumn::make('no')
-                    ->rowIndex(),
-                Tables\Columns\TextColumn::make('name')
-                    ->searchable()
-                    ->sortable(),
-                Tables\Columns\TextColumn::make('target')
-                    ->toggleable(isToggledHiddenByDefault: true)
-                    ->sortable()
-                    ->prefix(
-                        fn(Box $record): ?string => $record->target ? 'Rp ' : null
-                    )
-                    ->numeric(thousandsSeparator: '.')
-                    ->getStateUsing(
-                        fn(Box $record): int|string => $record->target ?? '-'
-                    ),
-                Tables\Columns\TextColumn::make('balance')
-                    ->sortable()
-                    ->prefix('Rp ')
-                    ->numeric(thousandsSeparator: '.'),
-                Tables\Columns\TextColumn::make('progress')
-                    ->sortable()
-                    ->badge(fn(Box $record): bool => (bool) $record->target)
-                    ->color('danger')
-                    ->state(function (Box $record): string {
-                        return $record->target
-                            ? $record->balance / $record->target * 100 . '%'
-                            : '-';
-                    }),
-                Tables\Columns\TextColumn::make('created_at')
-                    ->toggleable(isToggledHiddenByDefault: true)
-                    ->dateTime(),
-                Tables\Columns\TextColumn::make('updated_at')
-                    ->toggleable(isToggledHiddenByDefault: true)
-                    ->dateTime(),
-                Tables\Columns\TextColumn::make('deleted_at')
-                    ->toggleable(isToggledHiddenByDefault: true)
-                    ->dateTime()
-            ])
-            ->filters([
-                Tables\Filters\TrashedFilter::make(),
-            ])
-            ->actions([
-                Tables\Actions\DeleteAction::make(),
-                Tables\Actions\ForceDeleteAction::make(),
-                Tables\Actions\RestoreAction::make(),
-            ])
-            ->bulkActions([
-                Tables\Actions\BulkActionGroup::make([
-                    Tables\Actions\DeleteBulkAction::make(),
-                    Tables\Actions\ForceDeleteBulkAction::make(),
-                    Tables\Actions\RestoreBulkAction::make(),
-                ]),
-            ]);
-    }
-
+    
     public static function getEloquentQuery(): Builder
     {
         return parent::getEloquentQuery()
--- app/Filament/Resources/BoxResource/Pages/ListBoxes.php
+++ app/Filament/Resources/BoxResource/Pages/ListBoxes.php
@@ -5,6 +5,9 @@ namespace App\Filament\Resources\BoxResource\Pages;
 use App\Filament\Resources\BoxResource;
 use Filament\Actions;
 use Filament\Resources\Pages\ListRecords;
+use Filament\Tables;
+use Filament\Tables\Table;
+use App\Models\Box;
 
 class ListBoxes extends ListRecords
 {
@@ -15,5 +18,64 @@ class ListBoxes extends ListRecords
         return [
             Actions\CreateAction::make(),
         ];
+    }
+
+    public function table(Table $table): Table
+    {
+        return $table
+            ->columns([
+                Tables\Columns\TextColumn::make('no')
+                    ->rowIndex(),
+                Tables\Columns\TextColumn::make('name')
+                    ->searchable()
+                    ->sortable(),
+                Tables\Columns\TextColumn::make('target')
+                    ->toggleable(isToggledHiddenByDefault: true)
+                    ->sortable()
+                    ->prefix(
+                        fn(Box $record): ?string => $record->target ? 'Rp ' : null
+                    )
+                    ->numeric(thousandsSeparator: '.')
+                    ->getStateUsing(
+                        fn(Box $record): int|string => $record->target ?? '-'
+                    ),
+                Tables\Columns\TextColumn::make('balance')
+                    ->sortable()
+                    ->prefix('Rp ')
+                    ->numeric(thousandsSeparator: '.'),
+                Tables\Columns\TextColumn::make('progress')
+                    ->sortable()
+                    ->badge(fn(Box $record): bool => (bool) $record->target)
+                    ->color('danger')
+                    ->state(function (Box $record): string {
+                        return $record->target
+                            ? $record->balance / $record->target * 100 . '%'
+                            : '-';
+                    }),
+                Tables\Columns\TextColumn::make('created_at')
+                    ->toggleable(isToggledHiddenByDefault: true)
+                    ->dateTime(),
+                Tables\Columns\TextColumn::make('updated_at')
+                    ->toggleable(isToggledHiddenByDefault: true)
+                    ->dateTime(),
+                Tables\Columns\TextColumn::make('deleted_at')
+                    ->toggleable(isToggledHiddenByDefault: true)
+                    ->dateTime()
+            ])
+            ->filters([
+                Tables\Filters\TrashedFilter::make(),
+            ])
+            ->actions([
+                Tables\Actions\DeleteAction::make(),
+                Tables\Actions\ForceDeleteAction::make(),
+                Tables\Actions\RestoreAction::make(),
+            ])
+            ->bulkActions([
+                Tables\Actions\BulkActionGroup::make([
+                    Tables\Actions\DeleteBulkAction::make(),
+                    Tables\Actions\ForceDeleteBulkAction::make(),
+                    Tables\Actions\RestoreBulkAction::make(),
+                ]),
+            ]);
     }
 }
