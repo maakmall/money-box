5a4545c15c0b0a6f43fdd00f9398a95ee5f0d80b
Muhammad Akmal <makmal316@gmail.com>
2025-05-26 20:38:08 +0700

    fix: change box table layout to stack

--- app/Filament/Resources/BoxResource/Pages/ListBoxes.php
+++ app/Filament/Resources/BoxResource/Pages/ListBoxes.php
@@ -9,6 +9,8 @@ use Filament\Tables;
 use Filament\Tables\Table;
 use App\Models\Box;
 use Filament\Notifications\Notification;
+use Filament\Support\Enums\FontWeight;
+use Filament\Tables\Columns\TextColumn\TextColumnSize;
 use Illuminate\Support\HtmlString;
 
 class ListBoxes extends ListRecords
@@ -26,134 +28,136 @@ class ListBoxes extends ListRecords
     {
         return $table
             ->columns([
-                Tables\Columns\TextColumn::make('no')
-                    ->rowIndex(),
-                Tables\Columns\TextColumn::make('name')
-                    ->searchable()
-                    ->sortable(),
-                Tables\Columns\TextColumn::make('target')
-                    ->toggleable(isToggledHiddenByDefault: true)
-                    ->sortable()
-                    ->prefix(
-                        fn(Box $record): ?string => $record->target ? 'Rp ' : null
-                    )
-                    ->numeric(thousandsSeparator: '.')
-                    ->getStateUsing(
-                        fn(Box $record): int|string => $record->target ?? '-'
-                    ),
-                Tables\Columns\TextColumn::make('balance')
-                    ->sortable()
-                    ->prefix('Rp ')
-                    ->numeric(thousandsSeparator: '.'),
-                Tables\Columns\TextColumn::make('progress')
-                    ->sortable()
-                    ->badge(fn(Box $record): bool => (bool) $record->target)
-                    ->color(fn(string $state): string => match (true) {
-                        (int) substr($state, 0, -1) >= 100 => 'success',
-                        (int) substr($state, 0, -1) >= 75 => 'info',
-                        (int) substr($state, 0, -1) >= 50 => 'gray',
-                        (int) substr($state, 0, -1) >= 25 => 'warning',
-                        default => 'danger',
-                    })
-                    ->state(function (Box $record): string {
-                        return $record->target
-                            ? number_format($record->balance / $record->target * 100, 2) . '%'
-                            : '-';
-                    }),
-                Tables\Columns\TextColumn::make('created_at')
-                    ->toggleable(isToggledHiddenByDefault: true)
-                    ->dateTime(),
-                Tables\Columns\TextColumn::make('updated_at')
-                    ->toggleable(isToggledHiddenByDefault: true)
-                    ->dateTime(),
-                Tables\Columns\TextColumn::make('deleted_at')
-                    ->toggleable(isToggledHiddenByDefault: true)
-                    ->dateTime()
+                Tables\Columns\Layout\Stack::make([
+                    Tables\Columns\TextColumn::make('name')
+                        ->searchable()
+                        ->sortable()
+                        ->weight(FontWeight::Bold)
+                        ->size(TextColumnSize::Large)
+                        ->description(
+                            fn(Box $record): HtmlString => new HtmlString("$record->description<br><br>")
+                        ),
+                    Tables\Columns\Layout\Split::make([
+                        Tables\Columns\TextColumn::make('balance')
+                            ->sortable()
+                            ->getStateUsing(function(Box $record): string {
+                                $balance = number_format($record->balance, 0, ',', '.');
+                                
+                                if ($record->target) {
+                                    $target = number_format($record->target, 0, ',', '.');
+
+                                    return $balance . ' / ' . $target;
+                                }
+
+                                return $balance;
+                            }),
+                        Tables\Columns\TextColumn::make('progress')
+                            ->sortable()
+                            ->alignCenter()
+                            ->badge(fn(Box $record): bool => (bool) $record->target)
+                            ->color(fn(string $state): string => match (true) {
+                                (int) substr($state, 0, -1) >= 100 => 'success',
+                                (int) substr($state, 0, -1) >= 75 => 'info',
+                                (int) substr($state, 0, -1) >= 50 => 'gray',
+                                (int) substr($state, 0, -1) >= 25 => 'warning',
+                                default => 'danger',
+                            })
+                            ->state(function (Box $record): ?string {
+                                return $record->target
+                                    ? number_format($record->balance / $record->target * 100, 2) . '%'
+                                    : null;
+                            }),
+                    ]),
+                ])
+            ])
+            ->contentGrid([
+                'md' => 2,
+                'lg' => 3,
             ])
             ->filters([
                 Tables\Filters\TrashedFilter::make(),
             ])
             ->actions([
-                Tables\Actions\ActionGroup::make([
-                    Tables\Actions\DeleteAction::make()
-                        ->modalDescription(new HtmlString(
-                            'Are you sure you would like to do this?
+                Tables\Actions\ViewAction::make(),
+                Tables\Actions\EditAction::make(),
+                Tables\Actions\DeleteAction::make()
+                    ->modalDescription(new HtmlString(
+                        'Are you sure you would like to do this?
                             <br/>
                             <strong>
                             Make sure the balance in it is empty or moved to another box
                             </strong>
                             '
-                        ))
-                        ->successNotification(
-                            Notification::make()
-                                ->success()
-                                ->title('Box Deleted')
-                                ->body('The box has been deleted successfully')
-                        ),
-                    Tables\Actions\ForceDeleteAction::make()
-                        ->modalDescription(new HtmlString(
-                            'Are you sure you would like to do this?
+                    ))
+                    ->successNotification(
+                        Notification::make()
+                            ->success()
+                            ->title('Box Deleted')
+                            ->body('The box has been deleted successfully')
+                    ),
+                Tables\Actions\ForceDeleteAction::make()
+                    ->modalDescription(new HtmlString(
+                        'Are you sure you would like to do this?
                             <br/>
                             <strong>
                             This will delete your box and transaction history permanently
                             </strong>
                             '
-                        ))
-                        ->successNotification(
-                            Notification::make()
-                                ->success()
-                                ->title('Box Deleted')
-                                ->body('The box has been deleted permanently')
-                        ),
-                    Tables\Actions\RestoreAction::make()
-                        ->successNotification(
-                            Notification::make()
-                                ->success()
-                                ->title('Box Restored')
-                                ->body('The box has been restored successfully')
-                        )
-                ]),
+                    ))
+                    ->successNotification(
+                        Notification::make()
+                            ->success()
+                            ->title('Box Deleted')
+                            ->body('The box has been deleted permanently')
+                    ),
+                Tables\Actions\RestoreAction::make()
+                    ->successNotification(
+                        Notification::make()
+                            ->success()
+                            ->title('Box Restored')
+                            ->body('The box has been restored successfully')
+                    )
             ])
             ->bulkActions([
-                Tables\Actions\BulkActionGroup::make([
-                    Tables\Actions\DeleteBulkAction::make()
-                        ->modalDescription(new HtmlString(
-                            'Are you sure you would like to do this?
-                            <br/>
-                            <strong>
-                            Make sure the balance in it is empty or moved to another box
-                            </strong>
-                            '
-                        ))
-                        ->successNotification(
-                            Notification::make()
-                                ->success()
-                                ->title('Box Deleted')
-                                ->body('The box has been deleted successfully')
-                        ),
-                    Tables\Actions\ForceDeleteBulkAction::make()
-                        ->modalDescription(new HtmlString(
-                            'Are you sure you would like to do this?
-                            <br/>
-                            <strong>
-                            This will delete your box and transaction history permanently
-                            </strong>
-                            '
-                        ))
-                        ->successNotification(
-                            Notification::make()
-                                ->success()
-                                ->title('Box Deleted')
-                                ->body('The box has been deleted permanently')
-                        ),
-                    Tables\Actions\RestoreBulkAction::make()
-                        ->successNotification(
-                            Notification::make()
-                                ->success()
-                                ->title('Box Restored')
-                                ->body('The box has been restored successfully')
-                        )
-                ]),
+                // Tables\Actions\BulkActionGroup::make([
+                //     Tables\Actions\DeleteBulkAction::make()
+                //         ->modalDescription(new HtmlString(
+                //             'Are you sure you would like to do this?
+                //             <br/>
+                //             <strong>
+                //             Make sure the balance in it is empty or moved to another box
+                //             </strong>
+                //             '
+                //         ))
+                //         ->successNotification(
+                //             Notification::make()
+                //                 ->success()
+                //                 ->title('Box Deleted')
+                //                 ->body('The box has been deleted successfully')
+                //         ),
+                //     Tables\Actions\ForceDeleteBulkAction::make()
+                //         ->modalDescription(new HtmlString(
+                //             'Are you sure you would like to do this?
+                //             <br/>
+                //             <strong>
+                //             This will delete your box and transaction history permanently
+                //             </strong>
+                //             '
+                //         ))
+                //         ->successNotification(
+                //             Notification::make()
+                //                 ->success()
+                //                 ->title('Box Deleted')
+                //                 ->body('The box has been deleted permanently')
+                //         ),
+                //     Tables\Actions\RestoreBulkAction::make()
+                //         ->successNotification(
+                //             Notification::make()
+                //                 ->success()
+                //                 ->title('Box Restored')
+                //                 ->body('The box has been restored successfully')
+                //         )
+                // ]),
             ]);
     }
 }
