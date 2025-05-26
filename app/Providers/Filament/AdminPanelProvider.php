dcee124f3ab53203858736c57253c34209f87749
Muhammad Akmal <makmal316@gmail.com>
2025-05-26 19:45:15 +0700

    feat: add profile and register and use top navigation

--- app/Providers/Filament/AdminPanelProvider.php
+++ app/Providers/Filament/AdminPanelProvider.php
@@ -27,6 +27,9 @@ class AdminPanelProvider extends PanelProvider
             ->id('admin')
             ->spa()
             ->login()
+            ->registration()
+            ->profile()
+            ->topNavigation()
             ->colors([
                 'primary' => Color::Amber,
             ])
@@ -38,7 +41,6 @@ class AdminPanelProvider extends PanelProvider
             ->discoverWidgets(in: app_path('Filament/Widgets'), for: 'App\\Filament\\Widgets')
             ->widgets([
                 Widgets\AccountWidget::class,
-                Widgets\FilamentInfoWidget::class,
             ])
             ->middleware([
                 EncryptCookies::class,
