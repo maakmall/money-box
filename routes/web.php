613ebdf9ce89e2f3652bf2698d44889c221b5299
Muhammad Akmal <makmal316@gmail.com>
2025-06-26 13:23:20 +0700

    feat: add optimize route for application optimization

--- routes/web.php
+++ routes/web.php
@@ -1,1 +1,11 @@
 <?php
+
+use Illuminate\Support\Facades\Artisan;
+use Illuminate\Support\Facades\Route;
+
+Route::get('/optimize', function () {
+    Artisan::call('optimize');
+    Artisan::call('filament:optimize');
+
+    echo 'Optimized!';
+});
